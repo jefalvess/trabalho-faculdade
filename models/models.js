351981e75030aaff3ddce476a31862ba66a4561f
Jeferson Luis <jeferson.luis@g.globo>
2024-08-17 18:57:03 -0300

    a

--- models/models.js
+++ models/models.js
@@ -147,7 +147,7 @@ async function grupoPrivadoExecutar(bot) {
     const url = process.env.URL_GRUPO_PRIVADO;
     const fileName = "index1.html"
     await fetchAndSaveHtml(url, fileName);
-    const mensagens = await readAndLogHtmlFile(fileName, 6);
+    const mensagens = await readAndLogHtmlFile(fileName, 8);
     const chatId = parseInt(process.env.GRUPO_ID);
     const keys = await readAndLogMessages(mensagens, bot, chatId);
     // await excluirMensagens(bot, keys, chatId)
